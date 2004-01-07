585727551e5297916ab3aafe881fa70c6b0da9a4
learner <learner>
2004-01-07 20:25:31 +0000

    New patch level tagged

--- elc_private.h
+++ elc_private.h
@@ -5,11 +5,11 @@
 #define ELC_PRIVATE_H
 
 // VERSION DEFINITIONS
-#define VER_STRING	"0.9.4.1"
+#define VER_STRING	"0.9.4.2"
 #define VER_MAJOR	0
 #define VER_MINOR	9
 #define VER_RELEASE	4
-#define VER_BUILD	1
+#define VER_BUILD	2
 #define COMPANY_NAME	""
 #define FILE_VERSION	""
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
