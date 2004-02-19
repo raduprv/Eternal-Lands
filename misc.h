ea5c3fb43083ac57e23127f761f580257828071f
frak <frak>
2004-02-19 23:02:28 +0000

    dded a gtk fileselector

---
+++ misc.h
@@ -0,0 +1,14 @@
+
+
+void open_3d_obj();
+void open_2d_obj();
+void open_map_file();
+void save_map_file();
+
+#ifdef LINUX
+void open_3d_obj_continued();
+void open_2d_obj_continued();
+void open_map_file_continued();
+void save_map_file_continued();
+#endif
+
