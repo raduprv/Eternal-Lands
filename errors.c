14e3895979821a8728c4ae65adb4dfc7672d95ba
learner <learner>
2004-03-07 20:43:19 +0000

    Added log_error_details() and LogError macro, LogError changes if DEBUG defined

--- errors.c
+++ errors.c
@@ -1,34 +1,57 @@
 #include <string.h>
 #include "global.h"
 
+FILE *err_file = NULL;
 void clear_error_log()
 {
-	FILE *f = NULL;
-	f = fopen ("error_log.txt", "wb");
-	fclose (f);
+	if(!err_file) err_file = fopen ("error_log.txt", "wb");
+	fflush (err_file);
 }
 
-void log_error(char * message)
+void log_error(const Uint8 * message)
 {
-	FILE *f = NULL;
+	Uint8	str[2048];
+	Uint32	len;
 
-  	f = fopen ("error_log.txt", "ab");
-  	fwrite (message, strlen(message), 1, f);
-  	fclose (f);
+  	if(!err_file) err_file = fopen ("error_log.txt", "ab");
+	if(strncmp(message, "Error", 5))	// do we need to add Error:?
+		{
+			snprintf(str, 2048, "Error: %s\n", message);
+		}
+	else
+		{
+			snprintf(str, 2048, "%s\n", message);
+		}
+	len=strlen(str);
+	if(str[len-2] == '\n') len--;	// remove excess newline
+  	fwrite (str, len, 1, err_file);
+  	fflush (err_file);
 }
 
+void log_error_detailed(const Uint8 *message, const Uint8 *file, const Uint8 *func, Uint32 line)
+{
+	Uint8	str[2048];
+	Uint32	len;
+
+  	if(!err_file) err_file = fopen ("error_log.txt", "ab");
+	snprintf(str, 2048, "Error: %s.%s:%d - %s\n", file, func, line, message);
+	len=strlen(str);
+	if(str[len-2] == '\n') len--;	// remove excess newline
+  	fwrite (str, len, 1, err_file);
+  	fflush (err_file);
+}
+
+
+FILE *conn_file = NULL;
 void clear_conn_log()
 {
-	FILE *f = NULL;
-	f = fopen ("connection_log.txt", "wb");
-	fclose (f);
+	if(!conn_file) conn_file = fopen ("connection_log.txt", "wb");
+	fflush (conn_file);
 }
 
-void log_conn(unsigned char *in_data, int data_lenght)
+void log_conn(const Uint8 *in_data, Uint32 data_lenght)
 {
-	FILE *f = NULL;
-
-  	f = fopen ("connection_log.txt", "ab");
-  	fwrite (in_data, data_lenght, 1, f);
-  	fclose (f);
+  	if(!conn_file) conn_file = fopen ("connection_log.txt", "ab");
+  	fwrite (in_data, data_lenght, 1, conn_file);
+  	fflush (conn_file);
 }
--- errors.h
+++ errors.h
@@ -2,7 +2,15 @@
 #define __ERRORS_H__
 
 void clear_error_log();
-void log_error(char * message);
+void log_error(const Uint8 *message);
+void log_error_detailed(const Uint8 *message, const Uint8 *file, const Uint8 *function, Uint32 line);
 void clear_conn_log();
-void log_conn(unsigned char *in_data, int data_lenght);
+void log_conn(const Uint8 *in_data, Uint32 data_lenght);
+
+#ifdef	DEBUG
+#define	LogError(msg)	log_error_detailed(msg, __FILE__, __FUNCTION__, __LINE__)
+#else
+#define	LogError(msg)	log_error(msg)
+#endif	//DEBUG
+
 #endif
