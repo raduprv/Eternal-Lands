e89d0b5935b0985f4b4f3cb60d15429a679c5d0b
learner <learner>
2004-03-14 03:33:29 +0000

    Removed NV hack used for testing and dead code

--- font.c
+++ font.c
@@ -130,7 +130,6 @@ int	draw_char_scaled(unsigned char cur_char, int cur_x, int cur_y, float display
 	font_bit_width=get_font_width(chr);
 	displayed_font_x_width=(int)(0.5f+((float)font_bit_width)*displayed_font_x_size/12.0);
 	ignored_bits=(12-font_bit_width)/2;	// how many bits on each side of the char are ignored?
-	//ignored_bits=0;	
 
 	//now get the texture coordinates
 	u_start=(float)(row*font_x_size+ignored_bits)/256.0f;
@@ -162,13 +161,8 @@ void draw_string(int x, int y,unsigned char * our_string,int max_lines)
 
 void draw_string_zoomed(int x, int y,unsigned char * our_string,int max_lines, float text_zoom)
 {
-	//float u_start,u_end,v_start,v_end;
-	//int col,row;
 	float displayed_font_x_size=11.0*text_zoom;
 	float displayed_font_y_size=18.0*text_zoom;
-
-	//int font_x_size=FONT_X_SPACING;
-	//int font_y_size=FONT_Y_SPACING;
 
 	unsigned char cur_char;
 	int i;
@@ -220,13 +214,8 @@ void draw_string_zoomed(int x, int y,unsigned char * our_string,int max_lines, f
 
 void draw_string_small(int x, int y,unsigned char * our_string,int max_lines)
 {
-	//float u_start,u_end,v_start,v_end;
-	//int col,row;
 	int displayed_font_x_size=8;
 	int displayed_font_y_size=15;
-
-	//int font_x_size=FONT_X_SPACING;
-	//int font_y_size=FONT_Y_SPACING;
 
 	unsigned char cur_char;
 	int i;
@@ -263,49 +252,6 @@ void draw_string_small(int x, int y,unsigned char * our_string,int max_lines)
 					if(current_lines>=max_lines)break;
 					continue;
 				}
-			/*
-			else if(cur_char>=127)
-				{
-					float r,g,b;
-					//must be a color
-					cur_char-=127;
-					r=(float)colors_list[cur_char].r1/255.0f;
-					g=(float)colors_list[cur_char].g1/255.0f;
-					b=(float)colors_list[cur_char].b1/255.0f;
-					glColor3f(r,g,b);
-					i++;
-					continue;
-				}
-			else if(cur_char!='\n' && cur_char<FONT_START_CHAR)//invalid character
-				{
-					i++;
-					continue;
-				}
-			//first, see where that char is, in the font.bmp
-			cur_char-=FONT_START_CHAR;
-			col=cur_char/FONT_CHARS_PER_LINE;
-			row=cur_char%FONT_CHARS_PER_LINE;
-
-			//now get the texture coordinates
-			u_start=(float)(row*font_x_size)/256.0f;
-			u_end=(float)(row*font_x_size+font_x_size-7)/256.0f;
-			v_start=(float)1.0f-(1+col*font_y_size)/256.0f;
-			v_end=(float)1.0f-(col*font_y_size+font_y_size-2)/256.0f;
-
-			glTexCoord2f(u_start,v_start);
-			glVertex3i(cur_x,cur_y,0);
-
-			glTexCoord2f(u_start,v_end);
-			glVertex3i(cur_x,cur_y+displayed_font_y_size,0);
-
-			glTexCoord2f(u_end,v_end);
-			glVertex3i(cur_x+displayed_font_x_size,cur_y+displayed_font_y_size,0);
-
-			glTexCoord2f(u_end,v_start);
-			glVertex3i(cur_x+displayed_font_x_size,cur_y,0);
-
-			cur_x+=displayed_font_x_size;
-			*/
 
 			cur_x+=draw_char_scaled(cur_char, cur_x, cur_y, displayed_font_x_size, displayed_font_y_size);
 
@@ -365,11 +311,6 @@ void draw_ingame_string(float x, float y,unsigned char * our_string,
 	while(1)
 		{
 			cur_char=our_string[i];
-			//NV testing patch
-			if(cur_char == '-' && our_string[i+1])
-				{
-					cur_char=127+c_yellow2;
-				}
 			if(!cur_char)
 				{
 					break;
@@ -387,98 +328,6 @@ void draw_ingame_string(float x, float y,unsigned char * our_string,
 				{
 					glEnd();	//Ooops - NV bug fix!!
 				}
-			/*
-			if(cur_char>127+c_grey4)
-				{
-					if(cur_char==252)
-						cur_char=UUML;
-					else if(cur_char==233)
-						cur_char=EACUTE;
-					else if(cur_char==226)
-						cur_char=ACIRC;
-					else if(cur_char==224)
-						cur_char=AGRAVE;
-					else if(cur_char==231)
-						cur_char=CCEDIL;
-					else if(cur_char==234)
-						cur_char=ECIRC;
-					else if(cur_char==235)
-						cur_char=EUML;
-					else if(cur_char==232)
-						cur_char=EGRAVE;
-					else if(cur_char==239)
-						cur_char=IUML;
-					else if(cur_char==244)
-						cur_char=OCIRC;
-					else if(cur_char==251)
-						cur_char=UGRAVE;
-					else if(cur_char==230)
-						cur_char=aELIG;
-					else if(cur_char==248)
-						cur_char=oSLASH;
-					else if(cur_char==229)
-						cur_char=aRING;
-					else if(cur_char==198)
-						cur_char=AELIG;
-					else if(cur_char==216)
-						cur_char=OSLASH;
-					else if(cur_char==197)
-						cur_char=ARING;
-					else if(cur_char==228)
-						cur_char=aUMLAUT;
-					else if(cur_char==246)
-						cur_char=oUMLAUT;
-					else if(cur_char==252)
-						cur_char=uUMLAUT;
-					else if(cur_char==196)
-						cur_char=AUMLAUT;
-					else if(cur_char==214)
-						cur_char=OUMLAUT;
-					else if(cur_char==220)
-						cur_char=UUMLAUT;
-					else if(cur_char==223)
-						cur_char=DOUBLES;
-					else
-						{
-							i++;
-							continue;
-						}
-				}
-
-			if(!cur_char)break;
-			if(cur_char=='\n')
-				{
-					cur_y+=displayed_font_y_size;
-					cur_x=x;
-					i++;
-					current_lines++;
-					if(current_lines>=max_lines)break;
-					continue;
-				}
-			else if(cur_char>=127 && cur_char<=127+c_grey4)
-				{
-					float r,g,b;
-					//must be a color
-					cur_char-=127;
-					r=(float)colors_list[cur_char].r1/255.0f;
-					g=(float)colors_list[cur_char].g1/255.0f;
-					b=(float)colors_list[cur_char].b1/255.0f;
-					glColor3f(r,g,b);
-					i++;
-					continue;
-				}
-			else if(cur_char>=UUML && cur_char<=ARING)
-				{
-					cur_char-=(UUML-127);
-				}
-			else if(cur_char!='\n' && cur_char<FONT_START_CHAR)//invalid character
-				{
-					i++;
-					continue;
-				}
-			//first, see where that char is, in the font.bmp
-			cur_char-=FONT_START_CHAR;
-			*/
 			chr=find_font_char(cur_char);
 			if(chr >= 0)
 				{
@@ -498,18 +347,6 @@ void draw_ingame_string(float x, float y,unsigned char * our_string,
 					v_end=(float)1.0f-(col*font_y_size+font_y_size-1)/256.0f;
 					//v_end=(float)1.0f-(col*font_y_size+font_y_size-2)/256.0f;
 
-					/*glTexCoord2f(u_start,v_start);
-					glVertex3f(cur_x,cur_y,1+displayed_font_y_size);
-		
-					glTexCoord2f(u_start,v_end);
-					glVertex3f(cur_x,cur_y,1);
-
-					glTexCoord2f(u_end,v_end);
-					glVertex3f(cur_x+displayed_font_x_size,cur_y,1);
-
-					glTexCoord2f(u_end,v_start);
-					glVertex3f(cur_x+displayed_font_x_size,cur_y,1+displayed_font_y_size);
-					*/
 					glTexCoord2f(u_start,v_start);
 					glVertex3f(cur_x,0,cur_y+displayed_font_y_size);
 		
