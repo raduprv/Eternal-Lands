45d3f06305eea61f644aefea13c225516748e894
learner <learner>
2004-03-30 17:36:39 +0000

    Added to sync with ELC & fix compile bugs with clouds

---
+++ 3d_objects.h
@@ -0,0 +1,16 @@
+#ifndef __obj_3d_H__
+#define __obj_3d_H__
+
+void draw_3d_object(object3d * object_id);
+e3d_object * load_e3d_cache(char * file_name);
+int add_e3d(char * file_name, float x_pos, float y_pos, float z_pos, 
+			float x_rot, float y_rot, float z_rot, char self_lit, char blended,
+			float r, float g, float b);
+void display_objects();
+e3d_object * load_e3d(char *file_name);
+void compute_clouds_map(object3d * object_id);
+void clear_clouds_cache();
+void destroy_3d_object(int i);
+void flag_for_destruction();
+void destroy_the_flagged();
+#endif
