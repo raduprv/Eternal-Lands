307f5a3a6dcce5e3b1a1809ea9f9f73dbc4c54a5
cicero <cicero>
2004-04-12 15:54:50 +0000

    Updated knowledge window, now with ghetto scroll bar!

--- knowledge.c
+++ knowledge.c
@@ -5,7 +5,7 @@ _knowledge knowledge_list[200];
 
 char researching_name[16]="thingy";
 char researching_eta[10]="time";
-int page = 1;
+int page_start = 0;
 
 void display_knowledge()
 {
@@ -48,6 +48,9 @@ void display_knowledge()
 	glEnable(GL_TEXTURE_2D);
 	
 	draw_string(knowledge_menu_x+knowledge_menu_x_len-16,knowledge_menu_y+2,"X",1);
+	draw_string(knowledge_menu_x+knowledge_menu_x_len-16,knowledge_menu_y+18,"<",1);
+	draw_string(knowledge_menu_x+knowledge_menu_x_len-16,knowledge_menu_y+180,">",1);
+	draw_string(knowledge_menu_x+knowledge_menu_x_len-16,knowledge_menu_y+35+(130*page_start*256)/((200-76)*256),"*",1);
 	//draw text
 	draw_string_small(knowledge_menu_x+4,knowledge_menu_y+215,items_string,4);
 	glColor3f(1.0f,1.0f,1.0f);
@@ -56,7 +59,7 @@ void display_knowledge()
 	draw_string_zoomed(knowledge_menu_x+280,knowledge_menu_y+315,"ETA:",1,0.8);
 	draw_string_zoomed(knowledge_menu_x+320,knowledge_menu_y+315,researching_eta,1,0.8);
 	// Draw knowledges
-	for(i=(100*(page-1));i<100*page;i++){
+	for(i=page_start;i<page_start+76;i++){
 		knowledge_list[i].mouse_over ? glColor3f(0.77f,0.33f,0.33f) : glColor3f(0.9f,0.9f,0.9f);
 		draw_string_zoomed(x,y,knowledge_list[i].name,1,0.7);
 		x+=99;
@@ -75,7 +78,7 @@ int knowledge_mouse_over()
 	y=mouse_y-knowledge_menu_y;
 	x/=99;
 	y/=10;
-	knowledge_list[(100*(page-1))+y*4+x].mouse_over=1;
+	knowledge_list[page_start+y*4+x].mouse_over=1;
 	return 1;
 }
 
@@ -89,9 +92,23 @@ int check_knowledge_interface()
 
 	x=mouse_x-knowledge_menu_x;
 	y=mouse_y-knowledge_menu_y;
+	if(x > knowledge_menu_x_len-16 && x < knowledge_menu_x_len &&
+	   y > 18 && y < 18+16)
+		{
+			if(page_start > 0)
+				page_start -= 4;
+			return 1;
+		}
+	if(x > knowledge_menu_x_len-16 && x < knowledge_menu_x_len &&
+	   y > 180 && y < 180+16)
+		{
+			if(page_start < 200-76-4)
+				page_start += 4;
+			return 1;
+		}
 	x/=99;
 	y/=10;
-	idx = (100*(page-1))+y*4+x;
+	idx = page_start+y*4+x;
 	if(idx < 200 && *(knowledge_list[idx].name))
 		{
 			str[0] = RAW_TEXT;
