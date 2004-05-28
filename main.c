7ad9b5923962ecc72be5db98d2ac3242bc367a51
cicero <cicero>
2004-05-28 20:07:25 +0000

    fixed potential divide by zero

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ int start_rendering()
 					my_tcp_send(my_socket,&command,1);
 				}
 
-			if(!limit_fps || 800/(cur_time-last_time) < limit_fps)
+			if(!limit_fps || ((cur_time-last_time) && (800/(cur_time-last_time) < limit_fps)))
 				{
 					//draw everything
 					draw_scene();
