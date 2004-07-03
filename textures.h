091f5a09a02fc9ca19c04be1bb041e63310fe85a
learner <learner>
2004-07-03 02:35:55 +0000

    Sync of font & textures with map_editor

--- textures.h
+++ textures.h
@@ -8,10 +8,13 @@ char * load_bmp8_alpha_map(char * FileName);
 int load_texture_cache(char * file_name,unsigned char alpha);
 int		get_texture_id(int i);
 void	bind_texture_id(int texture_id);
-GLuint load_bmp8_remapped_skin(char * FileName, Uint8 a, short skin, short hair, short shirt,
+int		get_and_set_texture_id(int i);
+GLuint	load_bmp8_remapped_skin(char * FileName, Uint8 a, short skin, short hair, short shirt,
 							   short pants, short boots);
-void load_bmp8_to_coordinates(char * FileName, Uint8 *texture_space,int x_pos,int y_pos,
+void	load_bmp8_to_coordinates(char * FileName, Uint8 *texture_space,int x_pos,int y_pos,
 							  Uint8 alpha);
-int load_bmp8_enhanced_actor(enhanced_actor *this_actor, Uint8 a);
-int get_and_set_texture_id(int i);
+#ifdef	ELC
+int		load_bmp8_enhanced_actor(enhanced_actor *this_actor, Uint8 a);
+#endif	//ELC
+
 #endif
