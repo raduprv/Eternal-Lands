766d523154823c471ef1ac2457ba44301bc56939
cicero <cicero>
2004-07-05 21:54:07 +0000

    added some basic error checking

--- errors.c
+++ errors.c
@@ -5,6 +5,7 @@ void clear_error_log()
 	FILE *f = NULL;
 	char *file_name="error_log.txt";
 	f = fopen (file_name, "wb");
+	if(!f) return;
 	fclose (f);
 }
 
@@ -14,6 +15,7 @@ void log_error(char * message)
 	char *file_name="error_log.txt";
 
   	f = fopen (file_name, "a");
+	if(!f) return;
   	fwrite (message, strlen(message), 1, f);
   	fclose (f);
 }
