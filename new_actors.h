702331a156a35d28f9f04c06f49c5b0b44806cd9
cicero <cicero>
2004-08-11 01:00:32 +0000

    add prototype to fix compile warning

--- new_actors.h
+++ new_actors.h
@@ -8,5 +8,5 @@ void draw_enhanced_actor(actor * actor_id);
 void unwear_item_from_actor(int actor_id,Uint8 which_part);
 void actor_wear_item(int actor_id,Uint8 which_part, Uint8 which_id);
 void add_enhanced_actor_from_server(char * in_data);
-
+void build_glow_color_table();
 #endif
