9477684523eaaac7afca05bfc62fd8882dd1e8fb
learner <learner>
2004-09-06 02:54:45 +0000

    Removed some old code

--- events.c
+++ events.c
@@ -64,6 +64,7 @@ int HandleEvent(SDL_Event *event)
 			}
 			break;
 #endif
+
 	    case SDL_KEYDOWN:
 			{
 				key=(Uint16)event->key.keysym.sym;
@@ -553,19 +554,6 @@ int HandleEvent(SDL_Event *event)
 				{
 					if(left_click) end_drag_windows();
 					left_click = 0;
-					//attrib_menu_dragged=0;
-					//items_menu_dragged=0;
-					//ground_items_menu_dragged=0;
-					//manufacture_menu_dragged=0;
-					//trade_menu_dragged=0;
-					//sigil_menu_dragged=0;
-					//options_menu_dragged=0;
-					//dialogue_menu_dragged=0;
-					//knowledge_menu_dragged=0;
-					//encyclopedia_menu_dragged=0;
-					//buddy_menu_dragged=0;
-					//questlog_menu_dragged=0;
-					//knowledge_scroll_dragged=0;
 				}
 
 			if(event->type==SDL_MOUSEBUTTONDOWN) {
