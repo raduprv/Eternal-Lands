fbe8fd20b2e8d9a4d3d5d6ee15c62bb3962cb8c8
learner <learner>
2004-10-28 21:38:04 +0000

    Fixed animals getting the red tags (overlap with PKABLE info)

--- actors.c
+++ actors.c
@@ -213,7 +213,7 @@ void draw_actor_banner(actor * actor_id, float offset_z)
 					{
 						if(actor_id->kind_of_actor==NPC)glColor3f(0.3f,0.8f,1.0f);
 						else if(actor_id->kind_of_actor==HUMAN || actor_id->kind_of_actor==COMPUTER_CONTROLLED_HUMAN)glColor3f(1.0f,1.0f,1.0f);
-						else if(actor_id->kind_of_actor==PKABLE_HUMAN || actor_id->kind_of_actor==PKABLE_COMPUTER_CONTROLLED)glColor3f(1.0f,0.0f,0.0f);
+						else if(actor_id->is_enhanced_model && (actor_id->kind_of_actor==PKABLE_HUMAN || actor_id->kind_of_actor==PKABLE_COMPUTER_CONTROLLED))glColor3f(1.0f,0.0f,0.0f);
 						else glColor3f(1.0f,1.0f,0.0f);
 						//draw_ingame_string(-((float)get_string_width(actor_id->actor_name)*(SMALL_INGAME_FONT_X_LEN*zoom_level*name_zoom/3.0))/2.0/12.0,healtbar_z+(0.06f*zoom_level/3.0),actor_id->actor_name,1,0);
 						draw_ingame_small(-((float)get_string_width(actor_id->actor_name)*(SMALL_INGAME_FONT_X_LEN*zoom_level*name_zoom/3.0))/2.0/12.0,healtbar_z+(0.06f*zoom_level/3.0),actor_id->actor_name,1);
