f1df77803b4a9959103b303b9c286ee57e05487d
grum <grum>
2005-02-27 20:13:34 +0000

    Really add tabbed windows

---
+++ tabstats.h
@@ -0,0 +1,16 @@
+#ifndef __TABSTATS_H__
+#define __TABSTATS_H__
+
+#define TAB_WIDTH      580
+#define TAB_HEIGHT     350
+#define TAB_TAG_HEIGHT  20
+#define TAB_MARGIN       5
+
+extern int use_tabbed_windows;
+
+extern int tab_stats_win;
+extern int tab_collection_id;
+
+void display_tab_stats ();
+
+#endif // def __TABSTATS_H__
