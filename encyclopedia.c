82abdb3c93da9c262d923adb687f7263d6488955
wytter <wytter>
2005-07-13 22:33:09 +0000

    Adding vertical scrollbars to the Encyclopedia and help window - Thanks Zeplin

--- encyclopedia.c
+++ encyclopedia.c
@@ -8,6 +8,9 @@ int encyclopedia_menu_y=20;
 int encyclopedia_menu_x_len=500;
 int encyclopedia_menu_y_len=350;
 //int encyclopedia_menu_dragged=0;
+int encyclopedia_scroll_id=0;
+// Pixels to Scroll
+int encyclopedia_max_lines=1000;
 
 _Category Category[100];
 _Page Page[500];
@@ -19,55 +22,79 @@ int display_encyclopedia_handler(window_info *win)
 {
 	_Text *t=Page[currentpage].T.Next;
 	_Image *i=Page[currentpage].I.Next;
+	int j;
 
-	while(t){
+	j=vscrollbar_get_pos(encyclopedia_win,0);
+
+	while(t)
+	{
 		int ylen=(t->size)?18:15;
 		int xlen=strlen(t->text)*((t->size)?11:8);
 
-		if(t->ref)
-			{
-				//draw a line
-				glColor3f(0.5,0.5,0.5);
-				glDisable(GL_TEXTURE_2D);
-				glBegin(GL_LINES);
-				glVertex3i(t->x+4,t->y+ylen,0);
-				glVertex3i(t->x+4+xlen-8,t->y+ylen,0);
-				glEnd();
-				glEnable(GL_TEXTURE_2D);
-			}
-		if(t->size)
-			{
-				if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y) && mouse_y<(t->y+ylen+win->cur_y))
-				glColor3f(0.3,0.6,1.0);
-				else
-				glColor3f(t->r,t->g,t->b);
-				draw_string(t->x,t->y,t->text,1);
-			}
-		else
-			{
-				if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y) && mouse_y<(t->y+ylen+win->cur_y))
-				glColor3f(0.3,0.6,1.0);
-				else
-				glColor3f(t->r,t->g,t->b);
-				draw_string_small(t->x,t->y,t->text,1);
-			}
+		// Bounds Check the Text
+		if((t->y-j > 0) && (t->y-j < encyclopedia_menu_y_len-20 ))
+		{
+			if(t->ref)
+				{
+					//draw a line
+					glColor3f(0.5,0.5,0.5);
+					glDisable(GL_TEXTURE_2D);
+					glBegin(GL_LINES);
+					glVertex3i(t->x+4,t->y+ylen-j,0);
+					glVertex3i(t->x+4+xlen-8,t->y+ylen-j,0);
+					glEnd();
+					glEnable(GL_TEXTURE_2D);
+				}
+			if(t->size)
+				{
+					if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y-j) && mouse_y<(t->y+ylen+win->cur_y-j))
+					glColor3f(0.3,0.6,1.0);
+					else
+					glColor3f(t->r,t->g,t->b);
+					draw_string(t->x,t->y-j,t->text,1);
+				}
+			else
+				{
+					if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y-j) && mouse_y<(t->y+ylen+win->cur_y-j))
+					glColor3f(0.3,0.6,1.0);
+					else
+					glColor3f(t->r,t->g,t->b);
+					draw_string_small(t->x,t->y-j,t->text,1);
+				}
+		}
+		// No next line?
+		if(!t->Next)
+			break;
+
 		t=t->Next;
 	}
 
 	glColor3f(1.0f,1.0f,1.0f);
 	while(i){
-		if(i->mouseover==1){i=i->Next;continue;}
-		if(mouse_x>(i->x+win->cur_x) && mouse_x<(win->cur_x+i->xend) && mouse_y>(i->y+win->cur_y) && mouse_y<(win->cur_y+i->yend)){
-			if(i->Next!=NULL){
-				if(i->Next->mouseover==1)
-					i=i->Next;
+
+                // Bounds Check the Text
+                if((i->y-j > 0) && (i->yend-j < encyclopedia_menu_y_len-40 ))
+		{
+			if(i->mouseover==1)
+			{
+				i=i->Next;
+				continue;
 			}
+			if(mouse_x>(i->x+win->cur_x) && mouse_x<(win->cur_x+i->xend) && mouse_y>(i->y+win->cur_y-j) && mouse_y<(win->cur_y-j+i->yend))
+			{
+				if(i->Next!=NULL)
+				{
+					if(i->Next->mouseover==1)
+						i=i->Next;
+				}
+			}
+			get_and_set_texture_id(i->id);
+			glBegin(GL_QUADS);
+			draw_2d_thing(i->u, i->v, i->uend, i->vend,i->x, i->y-j,i->xend,i->yend-j);
+			glEnd();
 		}
-		get_and_set_texture_id(i->id);
-		glBegin(GL_QUADS);
-		draw_2d_thing(i->u, i->v, i->uend, i->vend,i->x, i->y,i->xend,i->yend);
-		glEnd();
 		i=i->Next;
+
 	}
 	return 1;
 }
@@ -75,13 +102,15 @@ int display_encyclopedia_handler(window_info *win)
 int click_encyclopedia_handler(window_info *win, int mx, int my, Uint32 flags)
 {
 	_Text *t=Page[currentpage].T.Next;
+        int j;
+        j=vscrollbar_get_pos(encyclopedia_win,0);
 
 	// only handle mouse button clicks, not scroll wheels moves
 	if ( (flags & ELW_MOUSE_BUTTON) == 0) return 0;
 
 	while(t){
 		int xlen=strlen(t->text)*((t->size)?11:8),ylen=(t->size)?18:15;
-		if(t->ref && mx>(t->x) && mx<(t->x+xlen) && my>(t->y) && my<(t->y+ylen)){
+		if(t->ref && mx>(t->x) && mx<(t->x+xlen) && my>(t->y-j) && my<(t->y+ylen-j)){
 				//changing page
 				int i;
 				for(i=0;i<numpage+1;i++){
@@ -580,5 +609,7 @@ void fill_encyclopedia_win ()
 {
 	set_window_handler (encyclopedia_win, ELW_HANDLER_DISPLAY, &display_encyclopedia_handler);
 	set_window_handler (encyclopedia_win, ELW_HANDLER_CLICK, &click_encyclopedia_handler);
+
+        vscrollbar_add_extended(encyclopedia_win, 0, NULL, encyclopedia_menu_x_len-20, 0, 20, encyclopedia_menu_y_len, 0, 1.0, 0.77f, 0.57f, 0.39f, 0, 1, encyclopedia_max_lines);
 }
 
--- help.c
+++ help.c
@@ -9,60 +9,77 @@ int help_menu_y=70;
 int help_menu_x_len=HELP_TAB_WIDTH;
 int help_menu_y_len=HELP_TAB_HEIGHT;
 
+// Pixels to Scroll
+int help_max_lines=1000;
+
 int helppage;
 
 int display_help_handler(window_info *win)
 {
 	_Text *t=Page[helppage].T.Next;
 	_Image *i=Page[helppage].I.Next;
+	int j;
+	j=vscrollbar_get_pos(help_win,0);
 
 	while(t){
 		int ylen=(t->size)?18:15;
 		int xlen=strlen(t->text)*((t->size)?11:8);
 
-		if(t->ref)
+		if((t->y-j > 0) && (t->y-j < help_menu_y_len-20 ))
+		{
+			if(t->ref)
 			{
 				//draw a line
 				glColor3f(0.5,0.5,0.5);
 				glDisable(GL_TEXTURE_2D);
 				glBegin(GL_LINES);
-				glVertex3i(t->x+4,t->y+ylen,0);
-				glVertex3i(t->x+4+xlen-8,t->y+ylen,0);
+				glVertex3i(t->x+4,t->y+ylen-j,0);
+				glVertex3i(t->x+4+xlen-8,t->y+ylen-j,0);
 				glEnd();
 				glEnable(GL_TEXTURE_2D);
 			}
-		if(t->size)
+			if(t->size)
 			{
-				if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y) && mouse_y<(t->y+ylen+win->cur_y))
+				if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y-j) && mouse_y<(t->y+ylen+win->cur_y-j))
 				glColor3f(0.3,0.6,1.0);
 				else
 				glColor3f(t->r,t->g,t->b);
-				draw_string(t->x,t->y,t->text,1);
+				draw_string(t->x,t->y-j,t->text,1);
 			}
-		else
+			else
 			{
-				if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y) && mouse_y<(t->y+ylen+win->cur_y))
+				if(t->ref && mouse_x>(t->x+win->cur_x) && mouse_x<(t->x+xlen+win->cur_x) && mouse_y>(t->y+win->cur_y-j) && mouse_y<(t->y+ylen+win->cur_y-j))
 				glColor3f(0.3,0.6,1.0);
 				else
 				glColor3f(t->r,t->g,t->b);
-				draw_string_small(t->x,t->y,t->text,1);
+				draw_string_small(t->x,t->y-j,t->text,1);
 			}
+		}
 		t=t->Next;
 	}
 
 	glColor3f(1.0f,1.0f,1.0f);
 	while(i){
-		if(i->mouseover==1){i=i->Next;continue;}
-		if(mouse_x>(i->x+win->cur_x) && mouse_x<(win->cur_x+i->xend) && mouse_y>(i->y+win->cur_y) && mouse_y<(win->cur_y+i->yend)){
-			if(i->Next!=NULL){
-				if(i->Next->mouseover==1)
-					i=i->Next;
+		if((i->y-j > 0) && (i->yend-j < help_menu_y_len-40 ))
+		{
+			if(i->mouseover==1)
+			{
+				i=i->Next;
+				continue;
 			}
+			if(mouse_x>(i->x+win->cur_x) && mouse_x<(win->cur_x+i->xend) && mouse_y>(i->y+win->cur_y-j) && mouse_y<(win->cur_y+i->yend-j))
+			{
+				if(i->Next!=NULL)
+				{
+					if(i->Next->mouseover==1)
+						i=i->Next;
+				}
+			}
+			get_and_set_texture_id(i->id);
+			glBegin(GL_QUADS);
+			draw_2d_thing(i->u, i->v, i->uend, i->vend,i->x, i->y-j,i->xend,i->yend-j);
+			glEnd();
 		}
-		get_and_set_texture_id(i->id);
-		glBegin(GL_QUADS);
-		draw_2d_thing(i->u, i->v, i->uend, i->vend,i->x, i->y,i->xend,i->yend);
-		glEnd();
 		i=i->Next;
 	}
 	return 1;
@@ -71,13 +88,16 @@ int display_help_handler(window_info *win)
 int click_help_handler(window_info *win, int mx, int my, Uint32 flags)
 {
 	_Text *t=Page[helppage].T.Next;
+	int j;
 
 	// only handle mouse button clicks, not scroll wheels moves
 	if ( (flags & ELW_MOUSE_BUTTON) == 0) return 0;
 
+	j=vscrollbar_get_pos(help_win,0);
+
 	while(t){
 		int xlen=strlen(t->text)*((t->size)?11:8),ylen=(t->size)?18:15;
-		if(t->ref && mx>(t->x) && mx<(t->x+xlen) && my>(t->y) && my<(t->y+ylen)){
+		if(t->ref && mx>(t->x) && mx<(t->x+xlen) && my>(t->y-j) && my<(t->y+ylen-j)){
 				//changing page
 				int i;
 				for(i=0;i<numpage+1;i++){
@@ -105,5 +125,6 @@ void fill_help_win ()
 	helppage=i;
 	set_window_handler (help_win, ELW_HANDLER_DISPLAY, &display_help_handler);
 	set_window_handler (help_win, ELW_HANDLER_CLICK, &click_help_handler);
-}
 
+	vscrollbar_add_extended(help_win, 0, NULL, help_menu_x_len-20, 0, 20, help_menu_y_len, 0, 1.0, 0.77f, 0.57f, 0.39f, 0, 1, help_max_lines);
+}
