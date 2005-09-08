3491bbf2ae78f9d8ef5ca1b24fa6871162e21967
wytter <wytter>
2005-09-08 17:49:38 +0000

    Umm, my brain is fried - reverting the last commit of this file, as it just re-introduced the problem...

--- tabs.c
+++ tabs.c
@@ -28,7 +28,7 @@ void display_tab_stats ()
 {
 	if (tab_stats_win < 0)
 	{
-		tab_stats_win = create_window ("Statistics", game_root_win, 0, tab_stats_x, tab_stats_y, tab_stats_len_x, tab_stats_len_y, ELW_WIN_DEFAULT);
+		tab_stats_win = create_window ("Statistics", -1, 0, tab_stats_x, tab_stats_y, tab_stats_len_x, tab_stats_len_y, ELW_WIN_DEFAULT);
 
 		set_window_handler (tab_stats_win, ELW_HANDLER_DISPLAY, &display_tab_stats_handler);
 		
