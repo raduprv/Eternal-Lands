32689e3658d8da2cfd9bdf152925656bd188fee1
wytter <wytter>
2005-10-13 19:55:53 +0000

    Version bump to 1.1.2

--- elc_private.h
+++ elc_private.h
@@ -5,10 +5,10 @@
 #define ELC_PRIVATE_H
 
 // VERSION DEFINITIONS
-#define VER_STRING	"1.1.0.0"
+#define VER_STRING	"1.1.2.0"
 #define VER_MAJOR	1
 #define VER_MINOR	1
-#define VER_RELEASE	0
+#define VER_RELEASE	2
 #define VER_BUILD	0
 #define COMPANY_NAME	""
 #define FILE_VERSION	""
@@ -18,6 +18,6 @@
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Eternal-Lands Client"
-#define PRODUCT_VERSION	"1.1.0"
+#define PRODUCT_VERSION	"1.1.2"
 
 #endif //ELC_PRIVATE_H
