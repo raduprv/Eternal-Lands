10825fe8687cd09eac1ceb50c9f2f5765d8ff26b
vegar <vegar>
2005-12-30 16:56:46 +0000

    Fixing bug #227: Mingw win32 build using #data_dir

--- books.c
+++ books.c
@@ -434,10 +434,18 @@ book * read_book(char * file, int type, int id)
 	book *b=NULL;
 	char path[1024];
 
+#ifndef WINDOWS
 	snprintf(path, sizeof(path), "%s/languages/%s/%s", datadir, lang, file);
+#else
+	snprintf(path, sizeof(path), "languages/%s/%s", lang, file);
+#endif // !WINDOWS
 
 	if ((doc = xmlReadFile(path, NULL, 0)) == NULL) {
+#ifndef WINDOWS
 		snprintf(path, sizeof(path), "%s/%s", datadir, file);
+#else
+		snprintf(path, sizeof(path), "%s", file);
+#endif // !WINDOWS
 		if((doc = xmlReadFile(path, NULL, 0)) == NULL) {
 			char str[200];
 
