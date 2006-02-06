3e24f823423de7efc8d688547559b9ca4839a012
learner <learner>
2006-02-06 22:26:04 +0000

    changed patch version

--- elc_private.h
+++ elc_private.h
@@ -5,19 +5,19 @@
 #define ELC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.2.0.0"
+#define VER_STRING	"1.2.0.1"
 #define VER_MAJOR	1
 #define VER_MINOR	2
 #define VER_RELEASE	0
-#define VER_BUILD	0
+#define VER_BUILD	1
 #define COMPANY_NAME	""
-#define FILE_VERSION	""
-#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define FILE_VERSION	"1.2.0p1"
+#define FILE_DESCRIPTION	"Eternal-Lands Client"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	""
-#define PRODUCT_NAME	""
-#define PRODUCT_VERSION	""
+#define ORIGINAL_FILENAME	"el.exe"
+#define PRODUCT_NAME	"elc"
+#define PRODUCT_VERSION	"1.2.0p1"
 
 #endif /*ELC_PRIVATE_H*/
