c2984ebdd50761bd3a032550457e47947220fc1d
learner <learner>
2006-03-04 15:13:37 +0000

    Patch to remove stuck ALT buttont during ALT-ENTER

--- gl_init.c
+++ gl_init.c
@@ -999,6 +999,7 @@ void toggle_full_screen()
 	set_new_video_mode(full_screen,video_mode);
 	build_video_mode_array();
     SDL_SetGamma(gamma_var, gamma_var, gamma_var);
+    SDL_SetModState(KMOD_NONE); // force ALL keys up
 }
 
 
