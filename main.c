ea16ca95d3875919694cab3ef2e93607446dc1e2
learner <learner>
2006-08-29 02:13:22 +0000

    protocol version update

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int client_version_minor=VER_MINOR;
 int client_version_release=VER_RELEASE;
 int	client_version_patch=VER_BUILD;
 int version_first_digit=10;	//protocol/game version sent to server
-int version_second_digit=15;
+int version_second_digit=16;
 
 int gargc;
 char **  gargv;
