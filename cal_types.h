74055d070ea5ac244a9255fd4cad6d4673e3e50b
learner <learner>
2007-02-17 19:18:00 +0000

    Code cleanup

--- cal_types.h
+++ cal_types.h
@@ -1,17 +1,20 @@
 #ifndef __CAL_TYPES_H__
 #define __CAL_TYPES_H__
 
+#ifdef NEW_SOUND
 #include "sound.h"
+#endif	//NEW_SOUND
+
 struct cal_anim
 {
-int anim_index;
-int kind;
-float duration;
+	int anim_index;
+	int kind;
+	float duration;
 #ifdef	NEW_ACTOR_ANIMATION
-float duration_scale;
+	float duration_scale;
 #endif
 #ifdef NEW_SOUND
-char sound[MAX_SOUND_NAME_LENGTH];
+	char sound[MAX_SOUND_NAME_LENGTH];
 #endif	//NEW_SOUND
 };
 
