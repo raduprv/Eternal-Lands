3daea4ff21554625798b67064d5d640bef158c47
learner <learner>
2007-02-22 15:27:41 +0000

    Added support for -DACTOR_ALPHA, but this could be made smarter still

--- cal.c
+++ cal.c
@@ -238,6 +238,13 @@ void cal_render_actor(actor *act)
 		// begin the rendering loop
 		if(CalRenderer_BeginRendering(pCalRenderer)){
 			// set global OpenGL states
+#ifdef	ALPHA_ACTORS
+			//glEnable(GL_ALPHA_TEST);
+			//glAlphaFunc(GL_GREATER,0.06f);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+			glDisable(GL_CULL_FACE);
+#endif	//ALPHA_ACTORS
 
 			// will use vertex arrays, so enable them
 			glEnableClientState(GL_VERTEX_ARRAY);
@@ -362,6 +369,11 @@ void cal_render_actor(actor *act)
 			glDisableClientState(GL_NORMAL_ARRAY);
 			glDisableClientState(GL_VERTEX_ARRAY);
 			glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+#ifdef	ALPHA_ACTORS
+			//glDisable(GL_ALPHA_TEST);
+			glEnable(GL_CULL_FACE);
+			glDisable(GL_BLEND);
+#endif	//ALPHA_ACTORS
 
 			// end the rendering
 			CalRenderer_EndRendering(pCalRenderer);
