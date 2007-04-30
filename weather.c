42efc885052fa8b0296801a66aff652e2717784b
ttlanhil <ttlanhil>
2007-04-30 12:39:28 +0000

    fix compile errors from karen's work

--- weather.c
+++ weather.c
@@ -619,14 +619,15 @@ float weather_bias_light(float value)
 		return 1.0f;
 	} else {
 		const float bias = get_weather_light_bias();
+		float result, severity;
 #ifdef NEW_LIGHTING
 		if (use_new_lighting)
-			const float severity = 0.9 * weather_severity; // Bias light by weather severity (the old way obliterates lighting, making everything but shadows black)
+			severity = 0.9 * weather_severity; // Bias light by weather severity (the old way obliterates lighting, making everything but shadows black)
 		else
 #else
-			const float severity = 0.15f*weather_severity + 0.85f; // slightly bias light by weather severity
+			severity = 0.15f*weather_severity + 0.85f; // slightly bias light by weather severity
 #endif
-		float result = value*(1.0f - severity*bias);
+		result = value*(1.0f - severity*bias);
 
 #ifdef NEW_LIGHTING
 		if (use_new_lighting)
@@ -634,9 +635,8 @@ float weather_bias_light(float value)
 			if ((result < 0.1f) && (result < value))
 				result = (value < 0.1 ? value : 0.1);
 		}
-		else
 #else
-			if (result < 0.0f) result = 0.0f;
+		if (result < 0.0f) result = 0.0f;
 #endif
 		else if (result > 1.0f) result = 1.0f;
 		
