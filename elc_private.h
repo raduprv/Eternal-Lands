022f936e0c40c6ec5f919440e39ba67d990722a0
learner <learner>
2007-06-03 19:01:03 +0000

    Version bump to 1.4.0

--- elc_private.h
+++ elc_private.h
@@ -5,19 +5,19 @@
 #define ELC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.3.3.0"
+#define VER_STRING	"1.4.0.0"
 #define VER_MAJOR	1
-#define VER_MINOR	3
-#define VER_RELEASE	3
+#define VER_MINOR	4
+#define VER_RELEASE	0
 #define VER_BUILD	0
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.3.3 i586"
+#define FILE_VERSION	"1.4.0"
 #define FILE_DESCRIPTION	"Eternal-Lands Client"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Eternal-Lands Client"
-#define PRODUCT_VERSION	"1.3.3"
+#define PRODUCT_VERSION	"1.4.0"
 
 #endif /*ELC_PRIVATE_H*/
