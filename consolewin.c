293fbff565aa7b5b4c298d18e97c42690e429094
ttlanhil <ttlanhil>
2007-06-17 10:33:41 +0000

    Possible fix for 2 bugs: Flicker in console when chat buffer full; Being away from end of console not honoured when chat buffer full

--- consolewin.c
+++ consolewin.c
@@ -43,6 +43,9 @@ void update_console_win (text_message * msg)
 			console_text_changed = 1;
 		} else {
 			scroll_up_lines += nlines;
+			if(scroll_up_lines > DISPLAY_TEXT_BUFFER_SIZE){
+				scroll_up_lines = DISPLAY_TEXT_BUFFER_SIZE;
+			}
 		}
 	}
 }
@@ -58,7 +61,7 @@ int display_console_handler (window_info *win)
 		set_font(chat_font);	// switch to the chat font
 		if (console_text_changed)
 		{
-			find_line_nr (total_nr_lines, total_nr_lines - nr_console_lines - scroll_up_lines, FILTER_ALL, &msg, &offset, chat_zoom, console_text_width);		
+			find_line_nr (total_nr_lines, total_nr_lines - nr_console_lines - scroll_up_lines, FILTER_ALL, &msg, &offset, chat_zoom, console_text_width);
 			text_field_set_buf_pos (console_root_win, console_out_id, msg, offset);
 			console_text_changed = 0;
 		}
