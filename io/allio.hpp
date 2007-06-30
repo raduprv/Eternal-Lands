a96f623f38c3dba2a6aefea0cca258712091d727
xaphier <xaphier>
2007-06-30 16:48:15 +0000

    * Added io/allio.hpp

---
+++ io/allio.hpp
@@ -0,0 +1,17 @@
+#ifndef	_ALLIO_HPP_
+#define	_ALLIO_HPP_
+
+#ifdef	USE_TR1
+#include <tr1/unordered_map>
+#else
+#include <map>
+#endif
+#include <vector>
+#include <string>
+#include <fstream>
+#include <zlib.h>
+#include <SDL.h>
+#include <sys/stat.h>
+#include "../exceptions/extendedexception.hpp"
+
+#endif	// _ALLIO_HPP_
