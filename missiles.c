5d0e63a9ca86aa79f1183f54d4e70922eb3a2dd7
schmurk <schmurk>
2008-01-03 23:10:19 +0000

    - modified a bit the layout of the code - added some comments in the header

--- missiles.c
+++ missiles.c
@@ -6,8 +6,8 @@
 #include "e3d.h"
 #include "gl_init.h"
 #include "missiles.h"
-#include "vmath.h"
 #include "tiles.h"
+#include "vmath.h"
 
 #include <assert.h>
 #include <math.h>
@@ -15,6 +15,31 @@
 
 #define MAX_MISSILES 1024
 #define EPSILON 1E-4
+
+#define MAT3_ROT_X(mat,angle) \
+(mat[0]=1.0,mat[3]=0.0       ,mat[6]=0.0        ,\
+ mat[1]=0.0,mat[4]=cos(angle),mat[7]=-sin(angle),\
+ mat[2]=0.0,mat[5]=-mat[7]   ,mat[8]=mat[4]     )
+
+#define MAT3_ROT_Y(mat,angle) \
+(mat[0]=cos(angle),mat[3]=0.0,mat[6]=sin(angle),\
+ mat[1]=0.0       ,mat[4]=1.0,mat[7]=0.0       ,\
+ mat[2]=-mat[6]   ,mat[5]=0.0,mat[8]=mat[0]    )
+
+#define MAT3_ROT_Z(mat,angle) \
+(mat[0]=cos(angle),mat[3]=-sin(angle),mat[6]=0.0,\
+ mat[1]=-mat[3]   ,mat[4]=mat[0]     ,mat[7]=0.0,\
+ mat[2]=0.0       ,mat[5]=0.0        ,mat[8]=1.0)
+
+#define MAT3_VECT3_MULT(res,mat,vect) \
+((res)[0]=mat[0]*(vect)[0]+mat[3]*(vect)[1]+mat[6]*(vect)[2],\
+ (res)[1]=mat[1]*(vect)[0]+mat[4]*(vect)[1]+mat[7]*(vect)[2],\
+ (res)[2]=mat[2]*(vect)[0]+mat[5]*(vect)[1]+mat[8]*(vect)[2])
+
+#define MAT3_MULT(res,mat1,mat2) \
+(MAT3_VECT3_MULT(&res[0],mat1,&mat2[0]),\
+ MAT3_VECT3_MULT(&res[3],mat1,&mat2[3]),\
+ MAT3_VECT3_MULT(&res[6],mat1,&mat2[6]))
 
 const float arrow_length = 7.0;
 const float arrow_color[3] = {0.8, 0.8, 0.8};
@@ -44,68 +69,77 @@ float get_actor_z(actor *a)
 	return -2.2f + height_map[a->y_tile_pos*tile_map_size_x*6+a->x_tile_pos]*0.2f;
 }
 
-void get_actor_bone_position(actor *a, int bone_id, float *pos, float *delta)
+void get_actor_rotation_matrix(actor *in_act, float *out_rot)
 {
-	float rotx_pos[3], roty_pos[3], rotz_pos[3];
-	float s_rx, c_rx, s_ry, c_ry, s_rz, c_rz;
-	float scale = get_actor_scale(a);
-	
+	float tmp_rot1[9], tmp_rot2[9];
+
+	MAT3_ROT_Z(out_rot, (180.0 - in_act->z_rot) * (M_PI / 180.0));
+	MAT3_ROT_X(tmp_rot1, in_act->x_rot * (M_PI / 180.0));
+	MAT3_MULT(tmp_rot2, out_rot, tmp_rot1);
+	MAT3_ROT_Y(tmp_rot1, in_act->y_rot * (M_PI / 180.0));
+	MAT3_MULT(out_rot, tmp_rot2, tmp_rot1);
+}
+
+void get_actor_bone_local_position(actor *in_act, int in_bone_id, float *in_shift, float *out_pos)
+{
 	struct CalSkeleton *skel;
 	struct CalBone *bone;
 	struct CalVector *point;
 
-	skel = CalModel_GetSkeleton(a->calmodel);
-	
-	bone = CalSkeleton_GetBone(skel, bone_id);
-
+	skel = CalModel_GetSkeleton(in_act->calmodel);
+	bone = CalSkeleton_GetBone(skel, in_bone_id);
 	point = CalBone_GetTranslationAbsolute(bone);
 
-	memcpy(pos, CalVector_Get(point), 3*sizeof(float));
+	memcpy(out_pos, CalVector_Get(point), 3*sizeof(float));
 
-	if (delta) {
+	if (in_shift) {
 		struct CalQuaternion *rot;
 		struct CalVector *vect;
 		float *tmp;
 		rot = CalBone_GetRotationAbsolute(bone);
 		vect = CalVector_New();
-		CalVector_Set(vect, delta[0], delta[1], delta[2]);
+		CalVector_Set(vect, in_shift[0], in_shift[1], in_shift[2]);
 		CalVector_Transform(vect, rot);
 		tmp = CalVector_Get(vect);
-		pos[0] += tmp[0];
-		pos[1] += tmp[1];
-		pos[2] += tmp[2];
+		out_pos[0] += tmp[0];
+		out_pos[1] += tmp[1];
+		out_pos[2] += tmp[2];
 		CalVector_Delete(vect);
 	}
+}
 
-	s_rx = sinf(a->x_rot * (M_PI / 180));
-	c_rx = cosf(a->x_rot * (M_PI / 180));
-	s_ry = sinf(a->y_rot * (M_PI / 180));
-	c_ry = cosf(a->y_rot * (M_PI / 180));
-	s_rz = sinf((180 -a->z_rot) * (M_PI / 180));
-	c_rz = cosf((180 -a->z_rot) * (M_PI / 180));
-	
-	rotz_pos[0] = pos[0] * c_rz - pos[1] * s_rz;
-	rotz_pos[1] = pos[0] * s_rz + pos[1] * c_rz;
-	rotz_pos[2] = pos[2];
-	
-	rotx_pos[0] = rotz_pos[0];
-	rotx_pos[1] = rotz_pos[1] * c_rx - rotz_pos[2] * s_rx;
-	rotx_pos[2] = rotz_pos[1] * s_rx + rotz_pos[2] * c_rx;
-	
-	roty_pos[0] = rotx_pos[2] * s_ry + rotx_pos[0] * c_ry;
-	roty_pos[1] = rotx_pos[1];
-	roty_pos[2] = rotx_pos[2] * c_ry - rotx_pos[0] * s_ry;
-	
-	pos[0] = roty_pos[0] * scale + a->x_pos + 0.25;
-	pos[1] = roty_pos[1] * scale + a->y_pos + 0.25;
-	pos[2] = roty_pos[2] * scale + get_actor_z(a);
+void transform_actor_local_position_to_absolute(actor *in_act, float *in_local_pos, float *in_act_rot, float *out_pos)
+{
+	float scale = get_actor_scale(in_act);
+
+	if (in_act_rot) {
+		MAT3_VECT3_MULT(out_pos, in_act_rot, in_local_pos);
+	}
+	else {
+		float rot[9];
+		get_actor_rotation_matrix(in_act, rot);
+		MAT3_VECT3_MULT(out_pos, rot, in_local_pos);
+	}
+
+	out_pos[0] = out_pos[0] * scale + in_act->x_pos + 0.25;
+	out_pos[1] = out_pos[1] * scale + in_act->y_pos + 0.25;
+	out_pos[2] = out_pos[2] * scale + get_actor_z(in_act);
+}
+
+void get_actor_bone_absolute_position(actor *in_act, int in_bone_id, float *in_shift, float *out_pos)
+{
+	float act_rot[9];
+	float pos[3];
+	get_actor_rotation_matrix(in_act, act_rot);
+	get_actor_bone_local_position(in_act, in_bone_id, in_shift, pos);
+	transform_actor_local_position_to_absolute(in_act, pos, act_rot, out_pos);
 }
 
 unsigned int add_missile(MissileType type,
 						 float origin[3],
 						 float target[3],
 						 float speed,
-						 float delta)
+						 float shift)
 {
 	Missile *mis;
 	
@@ -130,7 +164,7 @@ unsigned int add_missile(MissileType type,
 	mis->direction[2] /= mis->remaining_distance;
 	mis->speed = speed;
 	mis->covered_distance = 0;
-	mis->remaining_distance += delta;
+	mis->remaining_distance += shift;
 	
 	return missiles_count;
 }
@@ -150,16 +184,19 @@ void remove_missile(unsigned int missile_id)
 void draw_current_actor_nodes()
 {
 	actor *a = get_actor_ptr_from_id(yourself);
-	float pos[3], delta[3], tmp[3];
+	float act_rot[9];
+	float pos1[3], pos2[3], shift[3], tmp[3];
 	int i;
 	
 	if (!a) return;
 	
+	get_actor_rotation_matrix(a, act_rot);
+
 	glColor3f(1.0, 1.0, 1.0);
-	glLineWidth(3.0);
+	glLineWidth(5.0);
 	glBegin(GL_LINES);
 
- 	for (i = 37; i <= 37; ++i)
+ 	for (i = 29; i <= 29; ++i)
 	{
 /* 		switch(i) { */
 /* 		case 0: glColor3f(1.0, 0.0, 0.0); break; */
@@ -170,33 +207,30 @@ void draw_current_actor_nodes()
 /* 			glColor3f(1.0, 0.0, 0.0); */
 /* 		else */
 /* 			glColor3f(1.0, 1.0, 1.0); */
-/* 		get_actor_bone_position(a, i+30, pos, NULL); */
-/* 		glVertex3f(pos[0]-0.2, pos[1], pos[2]); */
-/* 		glVertex3f(pos[0]+0.2, pos[1], pos[2]); */
-/* 		glVertex3f(pos[0], pos[1]-0.2, pos[2]); */
-/* 		glVertex3f(pos[0], pos[1]+0.2, pos[2]); */
-/* 		glVertex3f(pos[0], pos[1], pos[2]-0.2); */
-/* 		glVertex3f(pos[0], pos[1], pos[2]+0.2); */
 
-		get_actor_bone_position(a, i, pos, NULL);
+		get_actor_bone_local_position(a, i, NULL, tmp);
+		transform_actor_local_position_to_absolute(a, tmp, act_rot, pos1);
 
-		delta[0] = 0.3; delta[1] = 0.0; delta[2] = 0.0;
-		get_actor_bone_position(a, i, tmp, delta);
+		shift[0] = 0.3; shift[1] = 0.0; shift[2] = 0.0;
+		get_actor_bone_local_position(a, i, shift, tmp);
+		transform_actor_local_position_to_absolute(a, tmp, act_rot, pos2);
 		glColor3f(1.0, 0.0, 0.0);
-		glVertex3fv(pos);
-		glVertex3fv(tmp);
+		glVertex3fv(pos1);
+		glVertex3fv(pos2);
 
-		delta[0] = 0.0; delta[1] = 0.3; delta[2] = 0.0;
-		get_actor_bone_position(a, i, tmp, delta);
+		shift[0] = 0.0; shift[1] = 0.3; shift[2] = 0.0;
+		get_actor_bone_local_position(a, i, shift, tmp);
+		transform_actor_local_position_to_absolute(a, tmp, act_rot, pos2);
 		glColor3f(0.0, 1.0, 0.0);
-		glVertex3fv(pos);
-		glVertex3fv(tmp);
+		glVertex3fv(pos1);
+		glVertex3fv(pos2);
 
-		delta[0] = 0.0; delta[1] = 0.0; delta[2] = 0.3;
-		get_actor_bone_position(a, i, tmp, delta);
+		shift[0] = 0.0; shift[1] = 0.0; shift[2] = 0.3;
+		get_actor_bone_local_position(a, i, shift, tmp);
+		transform_actor_local_position_to_absolute(a, tmp, act_rot, pos2);
 		glColor3f(0.0, 0.0, 1.0);
-		glVertex3fv(pos);
-		glVertex3fv(tmp);
+		glVertex3fv(pos1);
+		glVertex3fv(pos2);
 	}
 	
 	glEnd();
@@ -205,11 +239,11 @@ void draw_current_actor_nodes()
 void update_missiles(Uint32 time_diff)
 {
 	unsigned int i;
-	float delta_t = time_diff / 1000.0;
+	float shift_t = time_diff / 1000.0;
 	
 	for (i = 0; i < missiles_count; ) {
 		Missile *mis = &missiles_list[i];
-		float dist = mis->speed * delta_t;
+		float dist = mis->speed * shift_t;
 		mis->position[0] += mis->direction[0] * dist;
 		mis->position[1] += mis->direction[1] * dist;
 		mis->position[2] += mis->direction[2] * dist;
@@ -273,30 +307,30 @@ void draw_missiles()
 	glPopAttrib();
 }
 
-void shortest_arc(struct CalQuaternion *q, float from[3], float to[3])
+void shortest_arc(struct CalQuaternion *out_q, float *in_from, float *in_to)
 {
 	float cross[3];
 	float dot;
 
-	VCross(cross, from, to);
-	dot = VDot(from, to);
+	VCross(cross, in_from, in_to);
+	dot = VDot(in_from, in_to);
 
 	dot = (float)sqrt(2*(dot+1));
 	
 	VScale(cross, cross, 1/dot);
 	
-	CalQuaternion_Set(q, cross[0], cross[1], cross[2], -dot/2); 
+	CalQuaternion_Set(out_q, cross[0], cross[1], cross[2], -dot/2); 
 }
 
-float compute_actor_rotation(struct CalQuaternion *q, actor *a, float target[3])
+float compute_actor_rotation(struct CalQuaternion *out_q, actor *in_act, float *in_target)
 {
-	float cz = cosf((a->z_rot) * M_PI/180.0);
-	float sz = sinf((a->z_rot) * M_PI/180.0);
+	float cz = cosf((in_act->z_rot) * M_PI/180.0);
+	float sz = sinf((in_act->z_rot) * M_PI/180.0);
 	float from[3], to[3], tmp[3];
 	float actor_rotation;
 
-	tmp[0] = target[0] - a->x_pos;
-	tmp[1] = target[1] - a->y_pos;
+	tmp[0] = in_target[0] - in_act->x_pos;
+	tmp[1] = in_target[1] - in_act->y_pos;
 	tmp[2] = 0.0;
 	Normalize(tmp, tmp);
 
@@ -320,21 +354,21 @@ float compute_actor_rotation(struct CalQuaternion *q, actor *a, float target[3])
 	printf("actor rotation = %f\n", actor_rotation);
 #endif // DEBUG
 
-	cz = cosf((a->z_rot + actor_rotation) * M_PI/180.0);
-	sz = sinf((a->z_rot + actor_rotation) * M_PI/180.0);
+	cz = cosf((in_act->z_rot + actor_rotation) * M_PI/180.0);
+	sz = sinf((in_act->z_rot + actor_rotation) * M_PI/180.0);
 	from[0] = 0.0;
 	from[1] = 0.0;
 	from[2] = 1.0;
-	tmp[0] = target[1] - a->y_pos - 0.25;
-	tmp[1] = target[2] - get_actor_z(a) - 1.2;
-	tmp[2] = target[0] - a->x_pos - 0.25; // depends on the weapon
+	tmp[0] = in_target[1] - in_act->y_pos - 0.25;
+	tmp[1] = in_target[2] - get_actor_z(in_act) - 1.2;
+	tmp[2] = in_target[0] - in_act->x_pos - 0.25; // depends on the weapon
 	to[0] = tmp[0] * sz - tmp[2] * cz;
 	to[1] = tmp[1];
 	to[2] = tmp[0] * cz + tmp[2] * sz;
 
 	Normalize(to, to);
 
-	shortest_arc(q, from, to);
+	shortest_arc(out_q, from, to);
 
 	return actor_rotation;
 }
@@ -343,9 +377,9 @@ unsigned int fire_arrow(actor *a, float target[3])
 {
 	unsigned int mis_id;
 	float origin[3];
-	float delta[3] = {0.0, 0.4 * get_actor_scale(a), 0.0}; // depends on the weapon
+	float shift[3] = {0.0, 0.4 * get_actor_scale(a), 0.0}; // depends on the weapon
 
-	get_actor_bone_position(a, 37, origin, delta);
+	get_actor_bone_absolute_position(a, 37, shift, origin);
 	
 	mis_id = add_missile(MISSILE_ARROW, origin, target, 50.0, 0.0);
 	
--- missiles.h
+++ missiles.h
@@ -6,40 +6,149 @@
 #include "actors.h"
 #include "cal3d_wrapper.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum {MISSILE_ARROW} MissileType;
 
+/*!
+ * \brief Structure that handle flying missiles
+ */
 typedef struct
 {
-	MissileType type;
-	float position[3];
-	float direction[3];
-	float speed;
-	float covered_distance;
-	float remaining_distance;
+	MissileType type;   /*!< The type of the missile */
+	float position[3];  /*!< The position of the missile */
+	float direction[3]; /*!< The direction of the missile */
+	float speed;        /*!< The speed of the missile */
+	float covered_distance;   /*!< The distance covered by the missile */
+	float remaining_distance; /*!< The remaining distance to cover */
 } Missile;
 
+/*!
+ * \brief Removes all the missiles
+ */
 void clear_missiles();
 
+/*!
+ * \brief Adds a new missile
+ * \param type the type of the missile
+ * \param origin the origin of the missile
+ * \param target the target of the missile
+ * \param speed the speed of the missile
+ * \param shift allows to tune if the missile should stop before or after the target
+ */
 unsigned int add_missile(MissileType type,
 						 float origin[3],
 						 float target[3],
 						 float speed,
-						 float delta);
+						 float shift);
 
+/*!
+ * \brief Computes the next position for all missiles
+ */
 void update_missiles();
+
+/*!
+ * \brief Draws all the missiles
+ */
 void draw_missiles();
 
+/*!
+ * \brief Adds a new arrow (calls add_missile)
+ * \param a the actor throwing the arrow
+ * \param target the target
+ */
 unsigned int fire_arrow(actor *a, float target[3]);
 
-float compute_actor_rotation(struct CalQuaternion *q, actor *a, float target[3]);
+/*!
+ * \brief Computes the rotation matrix of an actor
+ * \param in_act the actor
+ * \param out_rot the resulting matrix (3x3 matrix: 9 floats)
+ */
+void get_actor_rotation_matrix(actor *in_act, float *out_rot);
+
+/*!
+ * \brief Gets the local position of char bone
+ * \param in_act the actor
+ * \param in_bone_id the bone
+ * \param in_shift a shift according to the orientation of the bone (can be NULL)
+ * \param out_pos the resulting position
+ */
+void get_actor_bone_local_position(actor *in_act, int in_bone_id, float *in_shift, float *out_pos);
+
+/*!
+ * \brief Transforms a local position on a char to an absolute position
+ * \param in_act the actor
+ * \param in_local_pos the local position
+ * \param in_act_rot the rotation matrix of the actor (computed inside if NULL)
+ * \param out_pos the resulting position
+ */
+void transform_actor_local_position_to_absolute(actor *in_act, float *in_local_pos, float *in_act_rot, float *out_pos);
+
+/*!
+ * \brief Gets the absolute position of char bone
+ * \param in_act the actor
+ * \param in_bone_id the bone
+ * \param in_shift a shift according to the orientation of the bone (can be NULL)
+ * \param out_pos the resulting position
+ */
+void get_actor_bone_absolute_position(actor *in_act, int in_bone_id, float *in_shift, float *out_pos);
+
+/*!
+ * \brief Computes the rotations to apply to a char when aiming something
+ * \param out_q the quaternion corresponding to a rotation between -22.5 and +22.5 degrees.
+ *              This quaternion has to be applied to bones of the char in order to have a fine orientation.
+ * \param in_act the actor
+ * \param in_target the target
+ * \return the rotation to apply to the whole char to face the target
+ */
+float compute_actor_rotation(struct CalQuaternion *out_q, actor *in_act, float *in_target);
+
+/*!
+ * \brief Rotates bones of an actor according to what is defined in its structure
+ * \param a the actor
+ */
 void rotate_actor_bones(actor *a);
 
-
+/*!
+ * \brief Tells an actor to aim at another actor
+ * \param actor1_id the ID of the current actor
+ * \param actor2_id the ID of the other actor
+ */
 void actor_aim_at_b(int actor1_id, int actor2_id);
+
+/*!
+ * \brief Tells an actor to aim at a target
+ * \param actor_id the ID of the actor
+ * \param target the target
+ */
 void actor_aim_at_xyz(int actor_id, float *target);
+
+/*!
+ * \brief Tells an actor to fire an arrow on another actor
+ * \param actor1_id the ID of the current actor
+ * \param actor2_id the ID of the other actor
+ */
 void missile_fire_a_to_b(int actor1_id, int actor2_id);
+
+/*!
+ * \brief Tells an actor to fire an arrow on a target
+ * \param actor_id the ID of the actor
+ * \param target the target
+ */
 void missile_fire_a_to_xyz(int actor_id, float *target);
+
+/*!
+ * \brief Fires a missile from a position to an actor
+ * \param origin the origin
+ * \param actor_id the actor
+ */
 void missile_fire_xyz_to_b(float *origin, int actor_id);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif // __MISSILES_H__
 
