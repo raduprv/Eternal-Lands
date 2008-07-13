851073f5d22cd50eb21eb1514d9ddf656ac3d14b
pjbroad <pjbroad>
2008-07-13 19:22:23 +0000

    Set version number to 1.7.0.  Set default #defs for 1.7.0

--- elc_private.h
+++ elc_private.h
@@ -5,19 +5,19 @@
 #define ELC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.6.0.0"
+#define VER_STRING	"1.7.0.0"
 #define VER_MAJOR	1
-#define VER_MINOR	6
+#define VER_MINOR	7
 #define VER_RELEASE	0
 #define VER_BUILD	0
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.6.0"
+#define FILE_VERSION	"1.7.0"
 #define FILE_DESCRIPTION	"Eternal-Lands Client"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Eternal-Lands Client"
-#define PRODUCT_VERSION	"1.6.0"
+#define PRODUCT_VERSION	"1.7.0"
 
 #endif /*ELC_PRIVATE_H*/
