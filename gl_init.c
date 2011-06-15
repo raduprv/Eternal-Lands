0529183093e90a71ecb55cb74f6533aebb895317
xaphier <el.3d.source@googlemail.com>
2011-06-15 23:11:24 +0200

    * FSAA mode check fix

--- gl_init.c
+++ gl_init.c
@@ -252,6 +252,7 @@ void check_gl_mode()
 
 void init_video()
 {
+	char str[400];
 	int rgb_size[3];
 
 	setup_video_mode(full_screen, video_mode);
@@ -327,6 +328,21 @@ void init_video()
 
 	SDL_WM_SetIcon(SDL_LoadBMP("icon.bmp"), NULL);
 	/* Set the window manager title bar */
+
+#ifdef	FSAA
+	if (fsaa > 1)
+	{
+		if (!SDL_SetVideoMode(window_width, window_height, bpp, flags))
+		{
+			safe_snprintf(str, sizeof(str), "Can't use fsaa mode x%d, disabling it.", fsaa);
+			LOG_TO_CONSOLE(c_yellow1, str);
+			LOG_WARNING("%s\n", str);
+			SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 0);
+			SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
+			fsaa = 0;
+		}
+	}
+#endif	/* FSAA */
 
 	//try to find a stencil buffer (it doesn't always work on Linux)
 	if(!SDL_SetVideoMode(window_width, window_height, bpp, flags))
