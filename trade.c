a4fca40eaf1b52f17685c74c896e29e2cd879276
Paul Broadhead <pjbroad@twinmoons.org.uk>
2013-02-19 21:46:32 +0000

    Use item_info module to display hover over descriptions of items in trade window.

--- trade.c
+++ trade.c
@@ -7,6 +7,7 @@
 #include "init.h"
 #include "interface.h"
 #include "items.h"
+#include "item_info.h"
 #include "manufacture.h"
 #include "multiplayer.h"
 #include "spells.h"
@@ -40,6 +41,7 @@ int trade_menu_x_len=9*33+20;
 int trade_menu_y_len=4*33+115;
 static int button_y_top = 4*33+40;
 static int button_y_bot = 4*33+60;
+static const char *tool_tip_str = NULL;
 
 
 int show_abort_help=0;
@@ -234,6 +236,13 @@ int display_trade_handler(window_info *win)
 		last_items_string_id = inventory_item_string_id;
 	}
 	draw_string_small(4,button_y_bot+5,(unsigned char*)items_string,3);
+
+	if (tool_tip_str != NULL)
+	{
+		show_help(tool_tip_str, 0, win->len_y+10);
+		tool_tip_str = NULL;
+	}
+
 #ifdef OPENGL_TRACE
 CHECK_GL_ERRORS();
 #endif //OPENGL_TRACE
@@ -362,8 +371,21 @@ int mouseover_trade_handler(window_info *win, int mx, int my)
 	if(mx>win->len_x-ELW_BOX_SIZE && my<ELW_BOX_SIZE) show_abort_help=1;
 	else show_abort_help=0;
 
-	if (show_help_text && *inventory_item_string && (my > button_y_bot+5)) {
-		show_help((disable_double_click)?click_clear_str :double_click_clear_str, 0, win->len_y+10);
+	if (show_help_text && *inventory_item_string && (my > button_y_bot+5))
+		tool_tip_str = (disable_double_click)?click_clear_str :double_click_clear_str;
+
+	else if (show_item_desc_text) {
+		trade_item *over_item = NULL;
+		int pos=get_mouse_pos_in_grid (mx, my, 4, 4, 10, 30, 33, 33);
+		if (pos >= 0 && your_trade_list[pos].quantity)
+			over_item = &your_trade_list[pos];
+		else {
+			int pos=get_mouse_pos_in_grid(mx, my, 4, 4, 10+5*33, 30, 33, 33);
+			if (pos >= 0 && others_trade_list[pos].quantity)
+				over_item = &others_trade_list[pos];
+		}
+		if ((over_item != NULL) && item_info_available() && (get_item_count(over_item->id, over_item->image_id) == 1))
+			tool_tip_str = get_item_description(over_item->id, over_item->image_id);
 	}
 
 	return 0;
