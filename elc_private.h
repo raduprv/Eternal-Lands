bb89115b5278ef98d1d026051427a6a6c0582ba1
Paul Broadhead <pjbroad@twinmoons.org.uk>
2015-04-07 23:04:38 +0100

    1.9.4 release, version bump and change log update.

--- elc_private.h
+++ elc_private.h
@@ -5,19 +5,19 @@
 #define ELC_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"1.9.3.0"
+#define VER_STRING	"1.9.4.0"
 #define VER_MAJOR	1
 #define VER_MINOR	9
-#define VER_RELEASE	3
+#define VER_RELEASE	4
 #define VER_BUILD	0
 #define COMPANY_NAME	""
-#define FILE_VERSION	"1.9.3"
+#define FILE_VERSION	"1.9.4"
 #define FILE_DESCRIPTION	"Eternal-Lands Client"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"Eternal-Lands Client"
-#define PRODUCT_VERSION	"1.9.3"
+#define PRODUCT_VERSION	"1.9.4"
 
 #endif /*ELC_PRIVATE_H*/
--- main.c
+++ main.c
@@ -85,7 +85,7 @@ int client_version_minor=VER_MINOR;
 int client_version_release=VER_RELEASE;
 int	client_version_patch=VER_BUILD;
 int version_first_digit=10;	//protocol/game version sent to server
-int version_second_digit=26;
+int version_second_digit=27;
 
 int gargc;
 char **  gargv;
