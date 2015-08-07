1157d311b6549631d925698a85acf62fe0732115
Paul Broadhead <pjbroad@twinmoons.org.uk>
2015-08-07 18:21:31 +0100

    Fix gcc v5 uninitialised warning, the code is unused.

--- io/normal.h
+++ io/normal.h
@@ -10,7 +10,7 @@
 
 #include <SDL_types.h>
 
-Uint16 compress_normal(const float *normal);
+/*Uint16 compress_normal(const float *normal);*/
 void uncompress_normal(const Uint16 value, float *normal);
 
 #endif	/* _NORMAL_H_ */
