72a7ddff60c395a05c50cbefb480b7bac5d58205
Gé Vissers <ge@octonion.nl>
2017-07-11 19:18:08 +0200

    Before logging in, the game time is unknown, and game minute is set to 0. However, new_second() is still called periodically (for whatever reason, and possibly only in the first minute, I haven't investigated). Combine this with the fact that is_day is initialized to 1, and we try to access the sun_show beyond its bounds if the user doesn't log in fast enough.
    Fix the out-of-bounds access by initializing is_day to 0. Perhaps we should
    also investigate if it's feasible to not call new_second() before login,
    as it seems rather pointless to update the sun's position with fake data.

--- shadows.c
+++ shadows.c
@@ -35,7 +35,7 @@ float ground_plane[4]={0,0,1,0};
 float sun_position[4]={400.0, 400.0, 500.0, 0.0};
 double light_view_mat[16],light_proj_mat[16],shadow_texgen_mat[16];
 int shadows_on=0;
-int is_day=1;
+int is_day=0;
 int use_shadow_mapping=1;
 
 //TODO: Would like to use TEXTURE_RECTANGLE for cards that support it, but for some reason it doesn't work??
