8661aba17a862cf6fd4e9ca97d3cc79b5964a48f
Paul Broadhead <pjbroad@twinmoons.org.uk>
2018-05-27 20:32:25 +0100

    Bump protocol version

--- main.c
+++ main.c
@@ -87,7 +87,7 @@ int client_version_minor=VER_MINOR;
 int client_version_release=VER_RELEASE;
 int	client_version_patch=VER_BUILD;
 int version_first_digit=10;	//protocol/game version sent to server
-int version_second_digit=27;
+int version_second_digit=28;
 
 int gargc;
 char **  gargv;
