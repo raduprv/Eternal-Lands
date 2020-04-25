1e23d350f86386fd531b47c3526cf28a238d78a7
Gé Vissers <gvissers@gmail.com>
2020-04-25 16:13:03 +0200

    Use reset_soft_breaks to wrap lines in the dialoges. This helps to properly wrap dialogue texts in variable width fonts.

--- dialogues.c
+++ dialogues.c
@@ -899,8 +899,14 @@ void display_dialogue(const Uint8 *in_data, int data_length)
 	if (dialogue_win >=0 && dialogue_win < windows_list.num_windows)
 		ui_scale_dialogue_handler(&windows_list.window[dialogue_win]);
 
-	// the window width is maintained during scaling so that the box is always available_text_width wide
-	put_small_text_in_box_zoomed(in_data, data_length, available_text_width * SMALL_FONT_X_LEN, (char*)dialogue_string, 1.0);
+	// the window width is maintained during scaling so that the box is always
+	// available_text_width wide
+	safe_strncpy2((char*)dialogue_string, (const char*)in_data, sizeof(dialogue_string),
+		data_length);
+	reset_soft_breaks((char *)dialogue_string, strlen((const char*)dialogue_string),
+		sizeof(dialogue_string), UI_FONT, DEFAULT_SMALL_RATIO,
+		available_text_width * SMALL_FONT_X_LEN, NULL, NULL);
+
 	recalc_option_positions = new_dialogue = new_text_to_log = 1;
 }
 
