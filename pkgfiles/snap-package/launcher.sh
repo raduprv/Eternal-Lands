5d42ff47cb67ba893174a7ba53fc83df78171ed6
Paul Broadhead <pjbroad@twinmoons.org.uk>
2020-07-12 13:30:45 +0100

    Update snap build. * Conditionally override TTF directory if not set or if not valid. Look in for fonts in several places to allow for different distros. * Bump runtime to core20.  This brings a newer version of libsdl2-ttf (amongst other updates) and so gives better rendering.

--- pkgfiles/snap-package/launcher.sh
+++ pkgfiles/snap-package/launcher.sh
@@ -31,4 +31,29 @@ then
 	cp -p $datadir/$inifile $userdir/$config/ || exit
 fi
 
-exec "$exename" -dir="$datadir" -b="$browser" -ttfdir=/usr/share/fonts/truetype "$config"
+# if no TTF specified, or not a valid path, try to override
+override_ttf=false
+fft_dir_line="$(grep "^#ttf_directory" $userdir/$config/$inifile)"
+if [ -z "$fft_dir_line" ]
+then
+	override_ttf=true
+else
+	ini_fft_path="$(echo "$fft_dir_line" | cut -d\= -f2 | cut -d\" -f2)"
+	[ -d "$ini_fft_path" ] || override_ttf=true
+fi
+
+# if overriding TTF, look for a suitable path
+ttfdir=""
+if $override_ttf
+then
+	for dir in 	"/usr/share/fonts/truetype/" "/usr/share/fonts/TTF/" "/usr/share/fonts/"
+	do
+		if [ -d "$dir" ]
+		then
+			ttfdir="-ttfdir=$dir"
+			break
+		fi
+	done
+fi
+
+exec "$exename" -dir="$datadir" -b="$browser" "$ttfdir" "$config"
