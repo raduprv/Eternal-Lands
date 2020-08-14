ab94cda4a456d6ac157061bedb6e125476c52306
Paul Broadhead <pjbroad@twinmoons.org.uk>
2020-08-14 15:03:49 +0100

    Fix summoning menu key #K_SUMMONINGMENU: now uses LALT+U. Previously we used CTRL+U but that was already in use for #K_USE.

--- keys.c
+++ keys.c
@@ -119,7 +119,7 @@ el_key_def K_WINSCALEUP = {SDLK_UP, KMOD_LCTRL, "#K_WINSCALEUP"};
 el_key_def K_WINSCALEDOWN = {SDLK_DOWN, KMOD_LCTRL, "#K_WINSCALEDOWN"};
 el_key_def K_WINSCALEDEF = {SDLK_HOME, KMOD_LCTRL, "#K_WINSCALEDEF"};
 el_key_def K_WINSCALEINIT = {SDLK_END, KMOD_LCTRL, "#K_WINSCALEINIT"};
-el_key_def K_SUMMONINGMENU = {SDLK_u, KMOD_LCTRL, "#K_SUMMONINGMENU"};
+el_key_def K_SUMMONINGMENU = {SDLK_u, KMOD_LALT, "#K_SUMMONINGMENU"};
 // Dont forget to add to key_def_store[]
 
 // Remaining keys are not assigned to the keyboard but
