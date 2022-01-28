d949e92bd8921525bcad225c9634ca90379aecfc
Gé Vissers <gvissers@gmail.com>
2022-01-28 18:17:49 +0100

    Add new water shader
    Add a new water shader which is compatible with GLSL 1.50. Some systems no longer support GLSL 1.40 and lower
    (specifically many of gl_* variables), and so cannot render the water reflection and rippling. The new shader
    should work on such systems. Keep the old shader around for ancient or crippled systems.

--- shader/shader.c
+++ shader/shader.c
@@ -18,23 +18,48 @@ typedef struct
 	const char* fragment_shader_defines;
 } shader_data;
 
+#define DEF_SHD     "#define USE_SHADOW\n"
+#define DEF_FOG     "#define USE_FOG\n"
+#define DEF_RFL     "#define USE_REFLECTION\n"
+#define DEF_CUB     "#define USE_CUBIC_FILTER\n"
+#define DEF_NOI     "#define USE_NOISE\n"
+#define VER_120     "#version 120\n"
+#define VER_150     "#version 150\n"
+
 shader_data shader_data_list[] = {
-	{ NULL, NULL, "./shaders/water_fs.glsl", " " },
-	{ NULL, NULL, "./shaders/water_fs.glsl", "#define\tUSE_SHADOW\n" },
-	{ NULL, NULL, "./shaders/water_fs.glsl", "#define\tUSE_FOG\n" },
-	{ NULL, NULL, "./shaders/water_fs.glsl", "#define\tUSE_FOG\n#define\tUSE_SHADOW\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_CUBIC_FILTER\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_CUBIC_FILTER\n#define\tUSE_SHADOW\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_CUBIC_FILTER\n#define\tUSE_FOG\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_CUBIC_FILTER\n#define\tUSE_FOG\n#define\tUSE_SHADOW\n" },
-	{ NULL, NULL, "./shaders/water_fs.glsl", "#define\tUSE_NOISE\n" },
-	{ NULL, NULL, "./shaders/water_fs.glsl", "#define\tUSE_NOISE\n#define\tUSE_SHADOW\n" },
-	{ NULL, NULL, "./shaders/water_fs.glsl", "#define\tUSE_NOISE\n#define\tUSE_FOG\n" },
-	{ NULL, NULL, "./shaders/water_fs.glsl", "#define\tUSE_NOISE\n#define\tUSE_FOG\n#define\tUSE_SHADOW\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_NOISE\n#define\tUSE_CUBIC_FILTER\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_NOISE\n#define\tUSE_CUBIC_FILTER\n#define\tUSE_SHADOW\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_NOISE\n#define\tUSE_CUBIC_FILTER\n#define\tUSE_FOG\n" },
-	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", "#define\tUSE_NOISE\n#define\tUSE_CUBIC_FILTER\n#define\tUSE_FOG\n#define\tUSE_SHADOW\n" }
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 },
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 DEF_SHD },
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 DEF_FOG },
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 DEF_FOG DEF_SHD },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_CUB },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_CUB DEF_SHD },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_CUB DEF_FOG },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_CUB DEF_FOG DEF_SHD },
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 DEF_NOI },
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 DEF_NOI DEF_SHD },
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 DEF_NOI DEF_FOG },
+	{ NULL, NULL, "./shaders/water_fs.glsl", VER_120 DEF_NOI DEF_FOG DEF_SHD },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_NOI DEF_CUB },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_NOI DEF_CUB DEF_SHD },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_NOI DEF_CUB DEF_FOG },
+	{ NULL, NULL, "./shaders/reflectiv_water_fs.glsl", VER_120 DEF_NOI DEF_CUB DEF_FOG DEF_SHD },
+
+	{ "./shaders/new_water.vert", VER_150, "./shaders/new_water.frag", VER_150 },
+	{ "./shaders/new_water.vert", VER_150 DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_SHD },
+	{ "./shaders/new_water.vert", VER_150 DEF_FOG, "./shaders/new_water.frag", VER_150 DEF_FOG },
+	{ "./shaders/new_water.vert", VER_150 DEF_FOG DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_FOG DEF_SHD },
+	{ "./shaders/new_water.vert", VER_150 DEF_RFL DEF_CUB, "./shaders/new_water.frag", VER_150 DEF_RFL DEF_CUB },
+	{ "./shaders/new_water.vert", VER_150 DEF_RFL DEF_CUB DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_RFL DEF_CUB DEF_SHD },
+	{ "./shaders/new_water.vert", VER_150 DEF_RFL DEF_CUB DEF_FOG, "./shaders/new_water.frag", VER_150 DEF_RFL DEF_CUB DEF_FOG },
+	{ "./shaders/new_water.vert", VER_150 DEF_RFL DEF_CUB DEF_FOG DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_RFL DEF_CUB DEF_FOG DEF_SHD },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI, "./shaders/new_water.frag", VER_150 DEF_NOI },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_NOI DEF_SHD },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI DEF_FOG, "./shaders/new_water.frag", VER_150 DEF_NOI DEF_FOG },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI DEF_FOG DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_NOI DEF_FOG DEF_SHD },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI DEF_RFL DEF_CUB, "./shaders/new_water.frag", VER_150 DEF_NOI DEF_RFL DEF_CUB },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI DEF_RFL DEF_CUB DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_NOI DEF_RFL DEF_CUB DEF_SHD },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI DEF_RFL DEF_CUB DEF_FOG, "./shaders/new_water.frag", VER_150 DEF_NOI DEF_RFL DEF_CUB DEF_FOG },
+	{ "./shaders/new_water.vert", VER_150 DEF_NOI DEF_RFL DEF_CUB DEF_FOG DEF_SHD, "./shaders/new_water.frag", VER_150 DEF_NOI DEF_RFL DEF_CUB DEF_FOG DEF_SHD },
 };
 
 #define	shader_data_size (sizeof(shader_data_list) / sizeof(shader_data))
@@ -286,6 +311,8 @@ static __inline__ int get_shader_index(shader_type type, shader_shadow_type shad
 	ret += fog_type * 2;
 	ret += shadow_type;
 	ret += max2i(min2i(quality, 1), 0) * 8;
+	if (max_supported_glsl_version() >= 150) // GLSL 1.5 remove gl_TexCoord, which was used in the old shader
+		ret += 16;
 
 	return ret;
 }
---
+++ shaders/new_water.frag
@@ -0,0 +1,105 @@
+in vec2 vs_tile_tex_coords;
+#ifdef USE_SHADOW
+in vec4 vs_pos_light_space;
+#endif // USE_SHADOW
+#ifdef USE_NOISE
+in vec3 vs_noise_tex_coords;
+#endif // USE_NOISE
+
+out vec4 frag_color;
+
+#ifdef USE_REFLECTION
+uniform vec4 viewport;
+#endif
+
+uniform sampler2D tile_texture;
+uniform float blend;
+
+#ifdef USE_REFLECTION
+uniform sampler2D reflection_texture;
+#endif // USE_REFLECTION
+
+#ifdef	USE_SHADOW
+uniform sampler2DShadow shadow_texture;
+uniform vec4 shadow_color;
+#endif	// USE_SHADOW
+
+#ifdef	USE_NOISE
+uniform sampler3D noise_texture;
+uniform vec4 noise_scale;
+#endif	// USE_NOISE
+
+#ifdef	USE_CUBIC_FILTER
+uniform sampler1D hg_texture;
+uniform vec2 size;
+uniform vec2 texel_size_x;
+uniform vec2 texel_size_y;
+#endif	// USE_CUBIC_FILTER
+
+#ifdef USE_FOG
+uniform vec4 fog_color;
+uniform float fog_density;
+#endif // USE_FOG
+
+void main (void)
+{
+	vec2 tile_tex_coords = vs_tile_tex_coords;
+	vec4 color, light;
+#ifdef USE_REFLECTION
+	vec2 reflection_tex_coords = (gl_FragCoord.xy - viewport.xy) / viewport.zw;
+#endif // USE_REFLECTION
+
+#ifdef USE_NOISE
+	vec2 noise_displacement = texture3D(noise_texture, vs_noise_tex_coords).ga * 2.0 - 1.0;
+ #ifdef USE_REFLECTION
+	reflection_tex_coords += noise_displacement * noise_scale.xy;
+ #endif // USE_REFLECTION
+	tile_tex_coords += noise_displacement * noise_scale.zw;
+#endif // USE_NOISE
+
+#ifdef USE_REFLECTION
+ #ifdef USE_CUBIC_FILTER
+	vec2 coord_hg = reflection_tex_coords * size - vec2(0.5, 0.5);
+	vec3 hg_x = texture(hg_texture, coord_hg.x).xyz;
+	vec3 hg_y = texture(hg_texture, coord_hg.y).xyz;
+
+	vec2 reflection_tex_coord10 = reflection_tex_coords + hg_x.x * texel_size_x;
+	vec2 reflection_tex_coord00 = reflection_tex_coords - hg_x.y * texel_size_x;
+	vec2 reflection_tex_coord11 = reflection_tex_coords + hg_y.x * texel_size_y;
+	vec2 reflection_tex_coord01 = reflection_tex_coords + hg_y.x * texel_size_y;
+	reflection_tex_coord10 -= hg_y.y * texel_size_y;
+	reflection_tex_coord00 -= hg_y.y * texel_size_y;
+
+	vec4 color00 = texture(reflection_texture, reflection_tex_coord00);
+	vec4 color10 = texture(reflection_texture, reflection_tex_coord10);
+	vec4 color01 = texture(reflection_texture, reflection_tex_coord01);
+	vec4 color11 = texture(reflection_texture, reflection_tex_coord11);
+
+	color00 = mix(color00, color01, hg_y.z);
+	color10 = mix(color10, color11, hg_y.z);
+	color = mix(color00, color10, hg_x.z);
+ #else // USE_CUBIC_FILTER
+	color = texture(reflection_texture, reflection_tex_coords);
+ #endif // USE_CUBIC_FILTER
+#else // USE_REFLECTION
+	color = vec4(1.0, 1.0, 1.0, 1.0);
+#endif // USE_REFLECTION
+
+#ifdef USE_SHADOW
+	float shadow = textureProj(shadow_texture, vs_pos_light_space);
+	light = vec4(1.0, 1.0, 1.0, 1.0); //gl_LightSource[7].ambient + gl_LightModel.ambient;
+	light = mix(shadow_color, light, shadow);
+#else // USE_SHADOW
+	light = vec4(1.0, 0.0, 1.0, 1.0);
+#endif // USE_SHADOW
+
+	frag_color = light * texture(tile_texture, tile_tex_coords);
+	frag_color = mix(frag_color, color, blend);
+
+#ifdef USE_FOG
+	const float LOG2 = 1.442695;
+	float fog = fog_density * gl_FragCoord.z / gl_FragCoord.w;
+	fog = exp2(-fog*fog*LOG2);
+	frag_color = mix(fog_color, frag_color, fog);
+#endif // USE_FOG
+}
